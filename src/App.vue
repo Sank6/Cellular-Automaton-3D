8ff4419e446ae384125144573865d407e2b832df
Sank6 <25014241+Sank6@users.noreply.github.com>
2021-06-08 18:26:29 +0100

    Add controls, colours and toggle dead cells

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import { Object3D, Color, DynamicDrawUsage, Vector3 } from 'three';
+import { Object3D, Color, DynamicDrawUsage, Matrix4 } from 'three';
 import Stats from 'stats.js';
 import * as dat from 'dat.gui';
 import {
@@ -64,16 +64,27 @@ export default {
       INC: 10,
       INIT: false,
       LOOP: true,
+      SHOW_DEAD: true,
+      ALIVE_COLOUR: "#ffc400",
+      DEAD_COLOUR: "#7b10e6",
       LEN,
       ARR
     };
   },
   mounted() {
     const gui = new dat.GUI();
-    gui.add(this, 'LEN', 8, this.MAX, 1).name("Size").onChange(this.updateLen)
-    gui.add(this, 'INC', 5, this.MAX, 1).name("Spacing").onChange(this.updateLen)
-    gui.add(this, 'SPEED', 0, 10).name("Speed").onChange(this.updateSpeed)
-    gui.add(this, 'LOOP').name("Loop").onChange(this.loop)
+    let options = gui.addFolder("Options");
+    options.add(this, 'LEN', 8, this.MAX, 1).name("Size").onChange(this.restart)
+    options.add(this, 'INC', 5, this.MAX, 1).name("Spacing").onChange(this.restart)
+    options.add(this, 'SPEED', 0, 10).name("Speed").onChange(this.updateSpeed)
+    options.add(this, 'LOOP').name("Loop").onChange(this.loop)
+    options.add(this, 'SHOW_DEAD').name("Show Dead Cells").onChange(this.replot)
+    let colours = gui.addFolder("Colours");
+    colours.addColor(this, 'ALIVE_COLOUR').name("Alive Cell")
+    colours.addColor(this, 'DEAD_COLOUR').name("Dead Cell")
+    let controls = gui.addFolder("Controls");
+    this.control = controls.add(this, 'stop').name("Pause")
+    controls.add(this, 'restart').name("Restart")
     
     this.stats = new Stats();
     this.stats.showPanel(0);
@@ -124,30 +135,58 @@ export default {
       x = parseInt(x);
       y = parseInt(y);
       z = parseInt(z);
-      let n = [];
+      let i = 0;
       for (let x1 = -1; x1 <= 1; x1 ++) {
         for (let y1 = -1; y1 <= 1; y1 ++) {
           for (let z1 = -1; z1 <= 1; z1 ++) {
-            n.push(this.ARR[x + x1]?.[y + y1]?.[z + z1])
+            if (this.ARR[x + x1]?.[y + y1]?.[z + z1] === true)
+              i++
           }
         }
       }
-      return n.filter(x => x).length;
+      return i;
     },
     animate() {
       this.stats.begin();
-
-      let i = 0;
-      for (let x in this.ARR) {
-        for (let y in this.ARR) {
-          for (let z in this.ARR) {
-            if (this.ARR[x][y][z]) {
-              this.imesh.setColorAt(i, new Color("#ffc400"))
-            } else
-              this.imesh.setColorAt(i, new Color("#7b10e6"))
-            i ++
+      if (this.SHOW_DEAD) {
+        let i = 0;
+        for (let x in this.ARR) {
+          for (let y in this.ARR) {
+            for (let z in this.ARR) {
+              if (this.ARR[x][y][z]) {
+                this.imesh.setColorAt(i, new Color(this.ALIVE_COLOUR))
+              } else {
+                this.imesh.setColorAt(i, new Color(this.DEAD_COLOUR))
+              }
+              i ++
+            }
           }
         }
+      } else {
+        let i = 0;
+        const dummy = new Object3D();
+        let m = new Matrix4();
+        m.set(0, 0, 0, 0,
+              0, 0, 0, 0,
+              0, 0, 0, 0,
+              0, 0, 0, 0 );
+        for (let x in this.ARR) {
+          for (let y in this.ARR) {
+            for (let z in this.ARR) {
+              if (this.ARR[x][y][z]) {
+                dummy.position.set((x - (this.LEN / 2)) * this.INC, (y - (this.LEN / 2)) * this.INC, (z - (this.LEN / 2)) * this.INC);
+                dummy.updateMatrix();
+                this.imesh.setMatrixAt(i, dummy.matrix);
+                this.imesh.setColorAt(i, new Color(this.ALIVE_COLOUR))
+              } else {
+                this.imesh.setMatrixAt(i, m);
+                this.imesh.setColorAt(i, new Color(this.DEAD_COLOUR))
+              }
+              i ++
+            }
+          }
+        }
+        this.imesh.instanceMatrix.needsUpdate = true;
       }
       this.imesh.instanceColor.needsUpdate = true;
       this.stats.end();
@@ -166,31 +205,67 @@ export default {
           for (let z in this.ARR) {
             let n = this.neighbours(x, y, z);
             if (!this.ARR[x][y][z]) {
-              if (n > 1 && n <= 2) todo.push([x, y, z])
-            } else {
-              if (n >= 3) {
-                this.ARR[x][y][z] = false;
+              if (n > 1 && n <= 2) {
+                todo.push([x, y, z])
+                count ++;
               }
+            } else {
+              if (n >= 3)
+                this.ARR[x][y][z] = false;
+              else
+                count ++;
             }
           }
         }
       }
-      for (let [x, y, z] of todo) {
+      for (let [x, y, z] of todo)
         this.ARR[x][y][z] = true;
-        if (this.ARR[x][y][z]) count ++;
-      }
+      todo = [];
       if (!count && this.LOOP) this.next = true;
     },
-    updateLen() {
+    restart() {
       this.ARR = Array.from(Array(this.LEN), () => {
         return Array.from(Array(this.LEN), () => new Array(this.LEN).fill(false))
       })
       this.start()
+      this.control.name("Pause")
     },
     updateSpeed() {
       this.TIME = 1 / (this.SPEED + 0.1);
-      window.clearInterval(this.interval);
-      this.interval = setInterval(this.step, this.TIME*1000)
+      if (this.interval) {
+        window.clearInterval(this.interval);
+        this.interval = setInterval(this.step, this.TIME*1000)
+      }
+    },
+    stop() {
+      if (this.interval) {
+        window.clearInterval(this.interval);
+        this.interval = null;
+        this.control.name("Resume")
+      } else {
+        this.control.name("Pause")
+        this.interval = setInterval(this.step, this.TIME*1000)
+      }
+    },
+    replot() {
+      const dummy = new Object3D();
+      let x,y,z;
+      x = y = z = - (this.LEN / 2) * this.INC;
+      for (let i = 0; i < (this.LEN)**3; i++) {
+        dummy.position.set(x, y, z);
+        dummy.updateMatrix();
+        this.imesh.setMatrixAt(i, dummy.matrix);
+        x += this.INC;
+        if (x == (this.LEN / 2) * this.INC) {
+          x = - (this.LEN / 2) * this.INC;
+          y += this.INC;
+        }
+        if (y == (this.LEN / 2) * this.INC) {
+          y = - (this.LEN / 2) * this.INC;
+          z += this.INC;
+        }
+      }
+      this.imesh.instanceMatrix.needsUpdate = true;
     }
   }
 };
